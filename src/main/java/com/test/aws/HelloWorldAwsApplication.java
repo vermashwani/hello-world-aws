a6a90ec62cb04889ba2dc0724667253b6ccbff86
ASHWANIVERMA <ashwani.verma@in.ibm.com>
2021-01-31 08:48:03 +0530

    updated

---
+++ src/main/java/com/test/aws/HelloWorldAwsApplication.java
@@ -0,0 +1,13 @@
+package com.test.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/aws/HelloWorldAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
