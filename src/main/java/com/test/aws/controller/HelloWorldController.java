87d9d62996ccb6170471a847e999865718a49dc4
ASHWANIVERMA <ashwani.verma@in.ibm.com>
2021-02-15 10:00:14 +0530

    updated

--- src/main/java/com/test/aws/controller/HelloWorldController.java
+++ src/main/java/com/test/aws/controller/HelloWorldController.java
@@ -2,13 +2,12 @@ package com.test.aws.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
@@ -17,71 +16,48 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 @RestController
-@RequestMapping("/")
 public class HelloWorldController {
 	
 	@GetMapping("/")
 	public String hello() {
 		return "Hello World!";
 	}
-	
+
 	@GetMapping("/welcome")
 	public String welcome(@RequestParam(value = "name", defaultValue = "in Spring Boot World") String name) {
-		return String.format("Welcome %s!",name);
+		return String.format("Welcome %s!", name);
 	}
-	
+
 	@GetMapping("/listbuckets")
-	public List<Bucket> getS3Buckets(){
-		Regions clientRegion = Regions.DEFAULT_REGION;
-        String bucketName = "*** Bucket name ***";
+	public ResponseEntity<?> getS3Buckets() {
 
-        AmazonS3 s3Client=null;
-        
-        try {
-			/*
-			 * s3Client = AmazonS3ClientBuilder.standard() .withCredentials(new
-			 * ProfileCredentialsProvider()) .withRegion(clientRegion) .build();
-			 */
-        	
-        	AWSCredentialsProvider provider = new AWSCredentialsProviderChain(new InstanceProfileCredentialsProvider(true),new ProfileCredentialsProvider());
-			s3Client  = AmazonS3ClientBuilder.standard().withRegion(clientRegion).withCredentials(provider).build();
+		ResponseEntity<?> responseEntity = null;
 
+		Regions clientRegion = Regions.US_EAST_2;
 
-            System.out.println("Listing objects"+s3Client.listBuckets());
+		AmazonS3 s3Client = null;
 
-            // maxKeys is set to 2 to demonstrate the use of
-            // ListObjectsV2Result.getNextContinuationToken()
-            ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withMaxKeys(2);
-            ListObjectsV2Result result;
+		System.out.println("Listing S3 Buckets---------->>");
+		try {
+			AWSCredentialsProvider provider = new AWSCredentialsProviderChain(
+					new InstanceProfileCredentialsProvider(true), new ProfileCredentialsProvider());
 
-            do {
-                result = s3Client.listObjectsV2(req);
+			s3Client = AmazonS3ClientBuilder.standard().withRegion(clientRegion).withCredentials(provider).build();
+			List<Bucket> buckets = s3Client.listBuckets();
+			
+			for(Bucket bucket : buckets) {
+			    System.out.println(bucket.getName());
+			}
 
-                for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-                    System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
-                }
-                // If there are more than maxKeys keys in the bucket, get a continuation token
-                // and list the next objects.
-                String token = result.getNextContinuationToken();
-                System.out.println("Next Continuation Token: " + token);
-                req.setContinuationToken(token);
-            } while (result.isTruncated());
-        } catch (AmazonServiceException e) {
-            // The call was transmitted successfully, but Amazon S3 couldn't process 
-            // it, so it returned an error response.
-            e.printStackTrace();
-        } catch (SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client
-            // couldn't parse the response from Amazon S3.
-            e.printStackTrace();
-        }
-        
-        return s3Client.listBuckets();
+			responseEntity = new ResponseEntity<List<Bucket>>(s3Client.listBuckets(), HttpStatus.OK);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			responseEntity = new ResponseEntity<String>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return responseEntity;
 	}
-
 }
